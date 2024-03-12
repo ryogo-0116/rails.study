e8257102cf1844e3d62372ef4ed2419e39cdb4a8
ryogo-0116 <ryogo01168812@gmail.com>
2024-03-12 20:13:34 +0900

    first commit

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,12 @@
+class LessonsController < ApplicationController
+  def hello
+    @hello = "こんにちは"
+    @message = "helloアクションが実行されています"
+  end
+
+  def call
+    @call = "callアクションが実行されています"
+  end
+  
+end
+
